f96674825571efbdea513481a7cde30a005c86d7
linzhongzi <7297317+linzhongzi@users.noreply.github.com>
2026-05-03 16:50:31 +0800

    修正版本号

--- EUD Editor 3/My Project/AssemblyInfo.vb
+++ EUD Editor 3/My Project/AssemblyInfo.vb
@@ -53,7 +53,7 @@ Imports System.Windows
 '
 ' 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로
 ' 지정되도록 할 수 있습니다.
-' <Assembly: AssemblyVersion("1.0.*")>
+' <Assembly: AssemblyVersion("0.19.*")>
 
 <Assembly: AssemblyVersion("0.19.6.0")>
 <Assembly: AssemblyFileVersion("0.19.6.0")>
